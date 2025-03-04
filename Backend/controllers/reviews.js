ea7079519af8dfd83f695dbdc2e23bc44c38b8b1
moamen ayman <moamenayman596@gmail.com>
2025-03-04 21:01:02 +0200

    add validations for user input

--- Backend/controllers/reviews.js
+++ Backend/controllers/reviews.js
@@ -1,63 +1,63 @@
-import Review from '../models/Review.js';
 import CustomError from '../helpers/CustomError.js';
-import { User } from '../models/Allusres.js';
-import Book from '../models/Book.js'
+import {User} from '../models/Allusres.js';
+import Book from '../models/Book.js';
+import Review from '../models/Review.js';
 
 const getReviews = async (bookId) => {
-    try{
-        const bookExists = await Book.findOne({book_id: bookId});
-        if(!bookExists) throw new CustomError("Book ID does not exist", 404);
-        
-        const reviews = await Review.find({book_id: bookId});
-        if(!reviews) throw new CustomError("Book does not has reviews"); 
-        return reviews;
-    }catch (error){
-        throw new CustomError(error.message || "Failed to get book review", error.status || 422);  
-    }
+  try {
+    const bookExists = await Book.findOne({book_id: bookId});
+    if (!bookExists) throw new CustomError('Book ID does not exist', 404);
+
+    const reviews = await Review.find({book_id: bookId});
+    if (!reviews) throw new CustomError('Book does not has reviews');
+    return reviews;
+  } catch (error) {
+    throw new CustomError(error.message || 'Failed to get book review', error.status || 422);
+  }
 };
 
 const addReview = async (data) => {
-  try{
-    const{user_id, book_id} = data;
+  try {
+    const {user_id, book_id} = data;
 
-    const userExists = await User.findOne({user_id: user_id});
-    if(!userExists) throw new CustomError("User ID does not exist", 404);
+    const userExists = await User.findOne({user_id});
+    if (!userExists) throw new CustomError('User ID does not exist', 404);
 
-    const bookExists = await Book.findOne({book_id: book_id});
-    if(!bookExists) throw new CustomError("Book ID does not exist", 404);
+    const bookExists = await Book.findOne({book_id});
+    if (!bookExists) throw new CustomError('Book ID does not exist', 404);
 
     const addedReview = await Review.create(data);
     return addedReview;
-   }catch (error){
-     throw new CustomError(error.message || "Failed to add review", error.status || 422);  
-   }
+  } catch (error) {
+    throw new CustomError(error.message || 'Failed to add review', error.status || 422);
+  }
 };
 
 const updateReview = async (id, body) => {
-    try{
-        const reviewExists = await Review.findOne({review_id: id});
-        if(!reviewExists) throw new CustomError("Review ID does not exist", 404);
-        const {rating, review} = body;
-        const updatedReview = await Review.findOneAndUpdate(
-          {review_id: id},
-          {rating, review},
-          {new: true}
-        );
-        return updatedReview;      
-    }catch (error){
-        throw new CustomError(error.message || "Failed to update review", error.status || 422);
-    }
+  try {
+    const reviewExists = await Review.findOne({review_id: id});
+    if (!reviewExists) throw new CustomError('Review ID does not exist', 404);
+    const {rating, review} = body;
+    const updatedReview = await Review.findOneAndUpdate(
+      {review_id: id},
+      {rating, review},
+      {new: true}
+    );
+    return updatedReview;
+  } catch (error) {
+    throw new CustomError(error.message || 'Failed to update review', error.status || 422);
+  }
 };
 
 const deleteReview = async (id) => {
-    try{
-        const reviewExists = await Review.findOne({review_id: id});
-        if(!reviewExists) throw new CustomError("Review ID does not exist", 404);
-        const deletedReview = await Review.findOneAndDelete({review_id: id});
-        return deletedReview;
-    }catch (error){
-        throw new CustomError(error.message || "Failed to delete review", error.status || 422);
-    }
+  try {
+    const reviewExists = await Review.findOne({review_id: id});
+    if (!reviewExists) throw new CustomError('Review ID does not exist', 404);
+    const deletedReview = await Review.findOneAndDelete({review_id: id});
+    return deletedReview;
+  } catch (error) {
+    throw new CustomError(error.message || 'Failed to delete review', error.status || 422);
+  }
 };
 
 export {addReview, deleteReview, getReviews, updateReview};
--- Backend/routes/Users.js
+++ Backend/routes/Users.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import {deleteUser, getUserProfile, loginUser, registerUser, updateUser} from '../controllers/Users.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import {registerValidation, updateValidation, validate} from '../validators/usersValidator.js';
 // import bcrypt from 'bcrypt';
 // import jwt from 'jsonwebtoken';
 // import {User , Customer , Admin} from '../models/Allusres.js';
 
 const router = express.Router();
 // login new user
-router.post('/', registerUser);
+router.post('/', validate(registerValidation), registerUser);
 // login user
 router.post('/login', loginUser);
 // get info abount current user
 router.get('/', authMiddleware, getUserProfile);
 // update info abount current user
-router.put('/', authMiddleware, updateUser);
+router.put('/', authMiddleware, validate(updateValidation), updateUser);
 // delete info abount current user
 router.delete('/', authMiddleware, deleteUser);
 
