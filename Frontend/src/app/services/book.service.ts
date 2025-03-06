a965ba61abffd7433b2ed829dbf97be5986c2c3c
youssef-medhatt <youssefmedhatt2001@gmail.com>
2025-03-06 20:24:29 +0200

    "generated book details list and card components, and generated book service and got the books using httpclient"

---
+++ Frontend/src/app/services/book.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+httpclient = inject(HttpClient);
+  constructor() { } 
+  getBooks(): Observable<any> {
+    return this.httpclient.get('http://localhost:3000/books');
+  }
+}
