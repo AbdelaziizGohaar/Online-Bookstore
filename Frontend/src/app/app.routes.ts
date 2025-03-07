94385ac76aebcb2f074a79829c20bd1fef68e910
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-03-07 22:09:57 +0200

    Create Not found page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
-
+import { NotfoundComponent } from './components/notfound/notfound.component'
 
 export const routes: Routes = [
     {
@@ -15,8 +15,13 @@ export const routes: Routes = [
         path: 'book-details/:id',
         component: BookDetailsComponent
     },
-
-    { path: 'login', 
-      component: LoginComponent 
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        component: NotfoundComponent,
+        title: 'Not Found Page'
     }
 ];
