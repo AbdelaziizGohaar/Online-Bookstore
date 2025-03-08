3f2b372ec9fc36f7be5926c2271489e75687965d
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-03-09 00:04:10 +0200

    Change the Profile component structure & Features

--- Frontend/src/app/components/profile/profile.component.ts
+++ Frontend/src/app/components/profile/profile.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  activeSection: string = 'account';
+
+  setActiveSection(section: string) {
+    this.activeSection = section;
+  }
 } 
