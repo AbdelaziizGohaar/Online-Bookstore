426632fca3c8ed3157e6acdeaed29e15d7f3c243
moamen ayman <moamenayman596@gmail.com>
2025-03-09 05:58:17 +0200

    add some validations

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -9,8 +9,12 @@ const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization');
     if (!token) throw new CustomError('Token not found', 401);
 
-    const verfiy = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verfiy;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      _id: decoded.userID,
+      user_id: decoded.user_id,
+      role: decoded.role
+    };
     next();
   } catch (error) {
     next(error);
