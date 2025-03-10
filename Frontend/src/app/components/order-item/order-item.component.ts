40c42c9705c061711df90dac5d808bb38dcd4e57
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-03-11 00:01:02 +0200

    Update the order.Components service, Dsscribtion ,item & list

--- Frontend/src/app/components/order-item/order-item.component.ts
+++ Frontend/src/app/components/order-item/order-item.component.ts
@@ -1,6 +1,8 @@
 import { Order } from '../../types/order';
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
+
 
 @Component({
   selector: 'app-order-item',
@@ -13,8 +15,18 @@ export class OrderItemComponent {
   @Input() order!: Order; // Input property to receive order data
   @Output() delete = new EventEmitter<number>(); // Output property to emit delete event
 
+  constructor(private router: Router) { } // Inject Router
+
+
   // Emit the delete event
   onDelete(): void {
     this.delete.emit(this.order.order_id);
   }
+
+  // Navigate to order details page
+  navigateToOrderDetails(): void {
+    this.router.navigate(['/orders', this.order.order_id]); // Navigate to order details
+  }
+
+
 }
