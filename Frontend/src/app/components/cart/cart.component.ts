348de31707bd5255c3be34cd8fb9a0b813f1ad18
Arwa Mohammed <arwamohmd.32@gmail.com>
2025-03-11 02:33:08 +0200

    dynamically display book details in cart component

--- Frontend/src/app/components/cart/cart.component.ts
+++ Frontend/src/app/components/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { Cart } from '../../types/cart';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../types/book';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -15,13 +17,40 @@ export class CartComponent {
   cartService = inject(CartService);
   cart!: Cart;
 
-  ngOnInit(): void {
+  bookService = inject(BookService);
+  books: { [key: number]: Book } = {};
+
+  ngOnInit(){
     this.cartService.getCartItems();
     this.cartService.cart$.subscribe((cart) => {
       if (cart) {
         this.cart = cart;
+        this.cart.arrayOfBooks.forEach((item) => {
+          this.bookService.getBookDetails(item.book_id).subscribe((data) => {
+            this.books[item.book_id] = {
+              ...data,
+              image: `http://localhost:3000${data.image}`
+            };
+          });
+        });
       }
     });
+  }
+
+  getSubtotal(): number {
+    if (!this.cart || !this.cart.arrayOfBooks) return 0;
+    return this.cart.arrayOfBooks.reduce((total, item) => {
+      const book = this.books[item.book_id];
+      if (book) {
+        return total + (book.price * item.booknum);
+      }
+      return total;
+    }, 0);
+  }
+  
+  shippingCost:number = 10.00;
+  getTotal(): number {  
+    return this.getSubtotal() + this.shippingCost;
   }
 
   updateItem(bookId: number, quantity: number): void {
