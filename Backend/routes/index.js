843aa6613b0f60b6800ee4926a0134938b367c35
Arwa Mohammed <arwamohmd.32@gmail.com>
2025-03-13 01:46:47 +0200

    Integrate a payment gateway (Stripe)

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import BooksRouter from './books.js';
 import CartRouter from './cart.js';
+import checkoutRouter from './checkout.js';
 import OrderRouter from './order.js';
 import ReviewsRouter from './reviews.js';
 import UsersRouter from './Users.js';
@@ -12,5 +13,6 @@ router.use('/reviews', ReviewsRouter);
 router.use('/cart', CartRouter);
 router.use('/orders', OrderRouter);
 router.use('/users', UsersRouter);
+router.use('/checkout', checkoutRouter);
 
 export default router;
