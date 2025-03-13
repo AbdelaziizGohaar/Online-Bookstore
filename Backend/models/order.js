26f823b69a6747bc20fcacfc0333aba32987677a
AbdelaziizGohaar <abdelazizgohaar25@gmail.com>
2025-03-13 18:56:54 +0200

    add a new component Checkout and Payment DB Model

--- Backend/models/order.js
+++ Backend/models/order.js
@@ -3,7 +3,47 @@ import mongooseSequence from 'mongoose-sequence';
 
 const AutoIncrement = mongooseSequence(mongoose); // Initialize it Autoincrement of Mongos
 
+const PaymentSchema = new mongoose.Schema({
+  payment_id: {
+    type: String // Provided by the payment gateway
+  },
+  paymentMethod: {
+    type: String,
+    required: true, // e.g., "Credit Card", "PayPal", "Stripe"
+    enum: ['credit_card', 'paypal', 'stripe']
+  },
+  amount_paid: {
+    type: Number,
+    required: true,
+    validate: {
+      validator(value) {
+        return value <= this.parent().totalPrice; // Ensure amount_paid <= totalPrice
+      },
+      message: 'Amount paid cannot exceed the total price.'
+    }
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'completed', 'failed'],
+    default: 'pending'
+  },
+  currency: {
+    type: String,
+    enum: ['usd', 'eur', 'gbp'], // Add supported currencies
+    default: 'usd'
+  },
+  paymentDate: {
+    type: Date,
+    default: null // Will be set when payment is completed
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 const OrderSchema = new mongoose.Schema({
+  Paymentdetails: PaymentSchema,
   order_id: {
     type: Number, // auto increment
     unique: true
@@ -69,3 +109,18 @@ OrderSchema.pre('save', async function (next) {
 
 const Order = mongoose.model('Order', OrderSchema);
 export default Order;
+
+// payment: {
+//     payment_id: {
+//       type: String, // Stripe Payment Intent ID
+//     },
+//     payment_method: {
+//       type: String, // e.g., card, bank transfer
+//     },
+//     amount_paid: {
+//       type: Number, // Total amount paid
+//     },
+//     currency: {
+//       type: String, // e.g., usd
+//     },
+//   }
