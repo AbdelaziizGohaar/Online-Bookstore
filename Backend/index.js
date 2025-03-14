f180b85905e80e625b5e0db25a57ac5e22a2b020
ElBanna532000 <mahmoudelbanna136@gmail.com>
2025-03-14 05:22:08 +0200

    made tiny changes on the book details template

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,7 @@ const logStream = fs.createWriteStream(path.join('logs', 'access.log'), {
 // Allow max 100 requests from the same IP address in one hour
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 100000,
   message: 'Too many requests from this IP, please try again later!'
 });
 
